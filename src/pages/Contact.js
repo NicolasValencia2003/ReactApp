83ffcd101a9cc9414be561bbb0cef4a1add679ab
Carlos Orduz <orduzguacaneme1@gmail.com>
2023-03-20 15:40:33 -0500

    cambios foro

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../App.css'
 
 
 const Forum = () => {
     return (
         <div className='ForumPage'>
-            <p>I am forum page</p>
+            <h2 className='nTitle'>Forum</h2>
+            <Link to=''>
+                <button className='botonChat1'>
+                    <img src='https://www.cuerpomente.com/medio/2019/07/03/semillas-de-chia-propiedades_76de1168_846x846.jpg' className='chat1Image'/>
+                </button>
+            </Link>
+            <h3>Que piensan de la chia?</h3>
+            <h5>Sabe a tierra con trigo pero es muy nutritiva segun los expertos de la DIAN</h5>
+            <h4>Hace 3 días por Pirilover69</h4>
+            <Link to=''>
+                <button className='botonChat2'>
+                    <img src='https://lamenteesmaravillosa.com/wp-content/uploads/2015/04/acosador.jpg' className='chat2Image'/>
+                </button>
+            </Link>
+            <h3>AYUDA!</h3>
+            <h5>Hay un acosador en mi gimnasio. Siempre que volteo, esta ahi mirandome seductoramente. Que hago?</h5>
+            <h4>Hace 4 días por ChicoPlay</h4>
+            <Link to=''>
+                <button className='BotonPublicar'>
+                    <h3>Publicar</h3>
+                </button>
+            </Link>
+
+            
         </div>
     )
 }

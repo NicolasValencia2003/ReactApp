7e02274f6957eeb2946ecf484622a72c9cfbb166
Carlos Orduz <orduzguacaneme1@gmail.com>
2023-03-21 10:53:08 -0500

    profile info

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css'
 
 
 const Profile = () => {
     return (
         <div className='ProfilePage'>
-            <p>I am profile page</p>
+            <h2 className='nTitle'>Profile</h2>
+            <Link to=''><button className='avatarButtom'><img src='https://img.freepik.com/free-icon/avatar_318-158392.jpg' className='AvatarImage' alt=''/></button></Link>
+            <h3>User Name</h3>
+            <h4>RandomUser123</h4>
+            <h3>Description</h3>
+            <h4>Hola soy nuevo en el gimnasio, espero aprender mucho y mejorar en mis ejercicios de pierna</h4>
+            
+
         </div>
     )
 }

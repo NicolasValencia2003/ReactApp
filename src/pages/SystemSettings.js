bb14fee2ba4d17001a07b8d4b90b139857e3ea4b
ssolarte-PUJ <ssolarteserna@javerianacali.edu.co>
2023-04-29 17:22:15 -0500

    Settings Page

--- src/pages/SystemSettings.js
+++ src/pages/SystemSettings.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './SystemSettings.css'
-
+import { FaCookie, FaEyeSlash, FaSun, FaVolumeUp, FaSave } from 'react-icons/fa';
 
 const SystemSettings = () => {
-    return (
-        <div className='SystemSettingsPage'>
-            <p>I am system settings page</p>
-        </div>
-    )
-}
+  return (
+    <div className='SystemSettings'>
+      <div className='SystemSettingsBox'>
+        <form>
+          <div className='SystemSettingsOption'>
+            <FaCookie className='SystemSettingsIcon' />
+            <label htmlFor='enable-cookies'>Enable cookies:</label>
+            <input type='checkbox' id='enable-cookies' name='enable-cookies' />
+          </div>
+
+          <div className='SystemSettingsOption'>
+            <FaEyeSlash className='SystemSettingsIcon' />
+            <label htmlFor='turn-off-tracking'>Turn off tracking:</label>
+            <input type='checkbox' id='turn-off-tracking' name='turn-off-tracking' />
+          </div>
+
+          <div className='SystemSettingsOption'>
+            <FaSun className='SystemSettingsIcon' />
+            <label htmlFor='brightness'>Brightness:</label>
+            <input type='range' id='brightness' name='brightness' min='0' max='100' />
+          </div>
+
+          <div className='SystemSettingsOption'>
+            <FaVolumeUp className='SystemSettingsIcon' />
+            <label htmlFor='volume'>Volume:</label>
+            <input type='range' id='volume' name='volume' min='0' max='100' />
+          </div>
+          <button className='SystemSettingsButton' type='submit'>
+            <FaSave className='SystemSettingsButtonIcon' />
+                Save
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default SystemSettings;

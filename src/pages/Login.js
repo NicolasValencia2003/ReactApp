436d8b149d52ca19304a5481d893259915a50cc0
Nicolas Valencia <nicolasvalencia@Nicolass-MacBook-Air.local>
2023-04-28 20:27:21 -0500

    NEW: Finished login verification and redirected to home if successful

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Login = () => {
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+    const handleNameChange = (event) => {
+        setName(event.target.value)
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+    const handleLogin = (event) => {
+        event.preventDefault()
+        const preloadUser = {name: 'ejemplo', password: 'hola123'}
+        if(name === preloadUser.name && password === preloadUser.password){
+            Swal.fire({
+                position: 'top',
+                icon: 'success',
+                title: 'Login Successful',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            navigate("/home")
+        }
+        else{
+            Swal.fire({
+                position: 'top',
+                icon: 'error',
+                title: 'Wrong Username or Password',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+    }
     return (
         <div className='LoginPage'>
             <h3>Inicio De Sesion!</h3>
             <h5>Ingrese sus datos</h5>
             <Link to='/'><button className='return'><img src='https://icons.veryicon.com/png/o/miscellaneous/energy-system-icon/return-57.png' className='returnImage' alt=''/></button></Link>
-            <form className = "nameInput">
-                <label>
-                    <h6>Nombre</h6>
-                    <input type="text" name="name" className = ""/>
-                </label>
+            <form onSubmit={handleLogin}>
+                <form className = "nameInput">
+                    <label>
+                        <h6>Nombre</h6>
+                        <input type="text" name="name" className = "" value={name} onChange={handleNameChange}/>
+                    </label>
+                </form>
+                <form className = "passwordInput">
+                    <label>
+                        <h6>Contraseña</h6>
+                        <input type="password" name="password" className = "" value={password} onChange={handlePasswordChange}/>
+                    </label>
+                </form>
+                    <button className='BotonIniciarHome' type='submit'>
+                        <p>Iniciar Sesion</p>
+                    </button>
             </form>
-            <form className = "passwordInput">
-                <label>
-                    <h6>Contraseña</h6>
-                    <input type="text" name="name" className = ""/>
-                </label>
-            </form>
-            <Link to='/home'>
-                <button className='BotonIniciarHome'>
-                    <p>Iniciar Sesion</p>
-                </button>
-            </Link>
 
         </div>
     );
